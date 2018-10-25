6828986f3b5f0ca17a3aa6eb34273ef5d5a27e07
Raffaele Zippo <raffaele.zippo@outlook.it>
2018-10-25 13:52:21 +0200

    Add script to compute fingerprints and write them on file

---
+++ Database/extract_fingerprints.ps1
@@ -0,0 +1,21 @@
+param(
+	[parameter(Mandatory=$true)][String]$sourceFolder,
+	[parameter(Mandatory=$true)][String]$outputFolder	
+)
+
+$files = Get-ChildItem -Path $sourceFolder -Recurse -Filter '*.wav';
+
+$total = $files.Length;
+$current = 1;
+foreach($file in $files)
+{
+    Write-Host "$current out of $total";
+    $current++;
+
+    $fingerprintGeneratorPath = "./fingerprint_generator.jar";
+    $fingerprintCmd = "java `"$fingerprintGeneratorPath`" `"$($file.FullName)`"";
+    $outfile = "$outputFolder/$($file.BaseName).fing.txt";
+    Invoke-Expression $fingerprintCmd | Out-File $outfile;
+}
+
+Write-Host "Finished."
